655b41c802a6a19a10b00c059c87ee12695bf61b
gahyeon <117976216+gahyeon11@users.noreply.github.com>
2024-08-07 15:48:03 +0900

    [Feat] Mates 페이지 개발 #32

--- src/pages/Mates.tsx
+++ src/pages/Mates.tsx
@@ -1,13 +1,71 @@
-import styled from 'styled-components';
+import React, { useState } from "react";
+import styled from "styled-components";
+import DiaryItem from "../components/diary/DiaryItem";
+import { dummyDiaries, dummyUsers, dummyLikedUsers } from "../dummyData";
+import MatesSidebar from "../components/sidebar/MatesSidebar";
 
-const Mates = () => {
+function Mates() {
+  const [likes, setLikes] = useState(
+    dummyDiaries.map((diary) => ({
+      id: diary.id,
+      likeCount: diary.like_count,
+    }))
+  );
+
+  const handleLikeChange = (diaryId: number, newLikeCount: number) => {
+    setLikes((prevLikes) =>
+      prevLikes.map((like) =>
+        like.id === diaryId ? { ...like, likeCount: newLikeCount } : like
+      )
+    );
+  };
+
   return (
     <MatesWrapper>
-      <h1>Mates</h1>
+      <MatesSidebar />
+      <MatesFeed>
+      {dummyDiaries.map((diary) => {
+        const diaryLikes = likes.find((like) => like.id === diary.id);
+        const user = dummyUsers.find((user) => user.user_id === parseInt(diary.user_id, 10)); // user_id를 number로 변환하여 비교
+        if (!user) {
+          return null; 
+        }
+        return (
+          <DiaryItemWrapper key={diary.id}>
+            <DiaryItem
+              diary={diary}
+              user={{
+                user_id: user.user_id,
+                profileImgURL: user.profileImgURL,
+                nickname: user.nickname,
+              }}
+              likedUsers={dummyLikedUsers}
+              isSummary={true}
+              likeCount={diaryLikes?.likeCount || 0}
+              setLikeCount={(value: React.SetStateAction<number>) =>
+                handleLikeChange(diary.id, typeof value === 'number' ? value : value(diaryLikes?.likeCount || 0))
+              }
+            />
+          </DiaryItemWrapper>
+        );
+      })}
+      </MatesFeed>
     </MatesWrapper>
-  )
-};
+  );
+}
 
-const MatesWrapper = styled.div``;
+const MatesWrapper = styled.div`
+  display: flex;
+  flex-direction: row; 
+  margin-bottom: 30px;
+`;
+
+const MatesFeed = styled.div`
+  position: relative;
+  top: 20px;
+`;
+
+const DiaryItemWrapper = styled.div`
+`
 
 export default Mates;
