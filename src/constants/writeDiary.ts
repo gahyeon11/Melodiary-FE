13feb1b136d9079a0d62c7b5a4d70414a911bb6c
“Dami-LEE00” <dxmxxii@gmail.com>
2024-08-07 15:10:55 +0900

    [Style] 일기 작성 페이지 배경 색상 경로 수정 #33

--- src/constants/writeDiary.ts
+++ src/constants/writeDiary.ts
@@ -1,15 +1,17 @@
+import { theme } from "../styles/theme";
+
 export const colors = [
-  { name: 'default', background: '#FFFFFF' },
-  { name: 'orange', background: '#FF87461A' },
-  { name: 'beige', background: '#FFD2791A' },
-  { name: 'yellow', background: '#FFDD2B1A' },
-  { name: 'green', background: '#8FEC471A' },
-  { name: 'mint', background: '#71F8C81A' },
-  { name: 'blue', background: '#9AD9EA1A' },
-  { name: 'coolblue', background: '#8AA3F91A' },
-  { name: 'purple', background: '#BA84FF1A' },
-  { name: 'pink', background: '#EA9ACA1A' },
-  { name: 'gray', background: '#9999991A' },
+  { name: 'default', background: theme.diaryColor.default.background },
+  { name: 'orange', background: theme.diaryColor.orange.background },
+  { name: 'beige', background: theme.diaryColor.beige.background },
+  { name: 'yellow', background: theme.diaryColor.yellow.background },
+  { name: 'green', background: theme.diaryColor.green.background },
+  { name: 'mint', background: theme.diaryColor.mint.background },
+  { name: 'blue', background: theme.diaryColor.blue.background },
+  { name: 'coolblue', background: theme.diaryColor.coolblue.background },
+  { name: 'purple', background: theme.diaryColor.purple.background },
+  { name: 'pink', background: theme.diaryColor.pink.background },
+  { name: 'gray', background: theme.diaryColor.gray.background },
 ];
 
 export const moods = ["😍", "😆", "🙂", "😟", "😡"];
