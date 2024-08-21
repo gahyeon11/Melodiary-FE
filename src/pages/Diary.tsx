5520d0869535a3dd95e0c58ab80b499bd1b0bfcb
gahyeon <117976216+gahyeon11@users.noreply.github.com>
2024-08-21 16:26:48 +0900

    [Design] Diary 페이지 CSS 수정 #30

--- src/pages/Diary.tsx
+++ src/pages/Diary.tsx
@@ -3,11 +3,12 @@ import { useParams } from "react-router-dom";
 import DiaryItem from "../components/diary/DiaryItem";
 import MusicBar from "../components/musicbar/MusicBar";
 import { useDiary } from "../hooks/useDiary";
+import styled from "styled-components";
 
 function Diary() {
-  const { diaryId } = useParams<{ diaryId: string }>(); 
+  const { diaryId } = useParams<{ diaryId: string }>();
   const numericDiaryId = diaryId ? Number(diaryId) : null;
-  
+
   const { diary, loading: diaryLoading, error } = useDiary(numericDiaryId!);
 
   if (diaryLoading) {
@@ -18,9 +19,8 @@ function Diary() {
     return <p>선택된 아이디의 다이어리 혹은 유저가 없습니다.</p>;
   }
 
-
   return (
-    <div>
+    <DiaryContainer backgroundColor={diary.body.background_color || undefined}>
       <DiaryItem
         diary={diary}
         user={diary.user_profile}
@@ -35,8 +35,19 @@ function Diary() {
           isExpanded={true}
         />
       )}
-    </div>
+    </DiaryContainer>
   );
 }
 
 export default Diary;
+
+const DiaryContainer = styled.div<{ backgroundColor?: string }>`
+  background-color: ${({ backgroundColor, theme }) =>
+    backgroundColor
+      ? theme.diaryColor[backgroundColor]?.background
+      : theme.color.highlight};
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
