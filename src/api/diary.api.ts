a0d0f693aa74ef0f16cb0449b0ee95ce2d78823a
“Dami-LEE00” <dxmxxii@gmail.com>
2024-08-24 19:25:55 +0900

    [Fix] 충돌 해결 후 나머지 파일 추가 머지 #66

--- src/api/diary.api.ts
+++ src/api/diary.api.ts
@@ -14,3 +14,25 @@ export const fetchDiaryById = async (diaryId: number): Promise<IDiary> => {
   const response = await httpClient.get<IDiary>(`/api/diaries/${diaryId}`);
   return response.data;
 };
+
+export const fetchPutDiary = async (diaryId: number, data: IDiary["body"]): Promise<IDiary> => {
+  try {
+    const response = await httpClient.put<IDiary>(`/api/diaries/${diaryId}`, data);
+    return response.data;
+  } catch (err) {
+    console.error("Failed to update diary", err);
+    throw err;
+  }
+};
+
+export const fetchDeleteDiary = async (diaryId: number) => {
+  try {
+    const response = await httpClient.delete(`/api/diaries/${diaryId}`);
+    return response.data;
+  } catch (err) {
+    console.log("일기 삭제에 실패했습니다.", err);
+    throw err;
+  }
+};
+
+
