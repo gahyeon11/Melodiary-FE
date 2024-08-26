0bcf4458328a69d62de1138a690874d8314f1775
gahyeon <117976216+gahyeon11@users.noreply.github.com>
2024-08-26 12:09:21 +0900

    [Fix] Mates피드 에러 로그 추가 #32

--- src/hooks/useMatesData.ts
+++ src/hooks/useMatesData.ts
@@ -2,12 +2,18 @@ import { useState, useEffect } from 'react';
 import { IDiary } from '../models/diary.model';
 import { fetchMateFeeds } from '../api/mates.api';
 
-export const useMatesData = (userId: number) => {
+export const useMatesData = (userId: number | undefined) => {
   const [diaries, setDiaries] = useState<IDiary[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
+    if (userId === undefined) {
+      setLoading(false);
+      setError(new Error("No user ID provided"));
+      return;
+    }
+
     const fetchData = async () => {
       try {
         const mateFeeds = await fetchMateFeeds(); 
