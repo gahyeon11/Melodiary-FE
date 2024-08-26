f826c35fc444623161e01746d694d2fe71d536e8
gahyeon <117976216+gahyeon11@users.noreply.github.com>
2024-08-26 12:07:09 +0900

    [Feat] 친구 삭제 API 호출 함수 추가 #32

--- src/api/mates.api.ts
+++ src/api/mates.api.ts
@@ -1,32 +1,46 @@
-import { IDiary } from '../models/diary.model';
-import { httpClient } from './http';
+import { IDiary } from "../models/diary.model";
+import { httpClient } from "./http";
 
 // 친구 피드 API 호출
 export const fetchMateFeeds = async (): Promise<IDiary[]> => {
-  const response = await httpClient.get<IDiary[]>('/api/diaries/mates');
+  const response = await httpClient.get<IDiary[]>("/api/diaries/mates");
   return response.data;
 };
 
-// 받은 친구 요청 목록을 가져오는 API 호출 
+// 받은 친구 요청 목록을 가져오는 API 호출
 export const fetchReceivedMateRequests = async (user_id: number) => {
-  const response = await httpClient.get(`/api/users/${user_id}/mates/requests/received`);
+  const response = await httpClient.get(
+    `/api/users/${user_id}/mates/requests/received`
+  );
   return response.data;
 };
 
-// 친구 요청을 수락하는 API 호출 
-export const acceptMateRequest = async (user_id: number,requestID: number) => {
-  const response = await httpClient.put(`/api/users/${user_id}/mates/requests/${requestID}`);
+// 친구 요청을 수락하는 API 호출
+export const acceptMateRequest = async (user_id: number, requestID: number) => {
+  const response = await httpClient.put(
+    `/api/users/${user_id}/mates/requests/${requestID}`
+  );
   return response.data;
 };
 
 // 친구 요청을 거절하는 API 호출
-export const rejectMateRequest = async (user_id: number,requestID: number) => {
-  const response = await httpClient.delete(`/api/users/${user_id}/mates/requests/${requestID}`);
+export const rejectMateRequest = async (user_id: number, requestID: number) => {
+  const response = await httpClient.delete(
+    `/api/users/${user_id}/mates/requests/${requestID}`
+  );
   return response.data;
 };
 
 // 친구 목록을 가져오는 API 호출
-export const fetchMatesList = async (user_id: number,) => {
+export const fetchMatesList = async (user_id: number) => {
   const response = await httpClient.get(`/api/users/${user_id}/mates`);
+  return response.data;
+};
+
+export const deleteMate = async (user_id: number, mate_id: number) => {
+  const response = await httpClient.delete(
+    `/api/users/${user_id}/mates/${mate_id}`, {
+      withCredentials: true,
+    });
   return response.data;
 };
